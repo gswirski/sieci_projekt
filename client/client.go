c24b574845023c2ca0a268cebca1b24609692453
kustosz <marcin.kostrzewa@llp.pl>
2015-01-27 14:56:07 +0100

    client can send a file

--- client/client.go
+++ client/client.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io"
 	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -13,5 +15,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Fprintf(conn, "Hello\n")
+	filename := os.Args[2]
+	file, err := os.Open(strings.TrimSpace(filename))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	fmt.Fprintf(conn, "ENDOFFILESEQYOULLNEVERUSEITINYOURCODE\n")
+	_, err = io.Copy(conn, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(conn, "\nENDOFFILESEQYOULLNEVERUSEITINYOURCODE\n")
+	conn.Close()
 }
